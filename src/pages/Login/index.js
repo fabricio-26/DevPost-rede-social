cd36c1dcfbc7724f83730fc1caada0f36bbc45c0
fabricio-26 <fabricioohh@gmail.com>
2022-11-29 13:44:53 -0300

    Pagina de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+
+import { Container, Title, Input, Button, ButtonText, SignUpButton, SignUpText } from './styles'
 
 export default function Login() {
- return (
-   <View>
-    <Text>Tela Login</Text>
-   </View>
+  const [login, setLogin] = useState(false);
+
+  function toggleLogin(){
+    setLogin(!login)
+  }
+
+  if(login){
+    return (
+      <Container>
+        <Title>
+          Dev<Text style={{ color:"#E52246" }}>Post</Text>
+        </Title>
+  
+        <Input
+          placeholder="Seuemail@teste.com"
+        />
+  
+        <Input
+          placeholder="*****"
+        />
+  
+        <Button>
+          <ButtonText>Acessar</ButtonText>
+        </Button>
+  
+        <SignUpButton onPress={toggleLogin}>
+          <SignUpText>Criar uma conta</SignUpText>
+        </SignUpButton>
+  
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Title>
+        Dev<Text style={{ color:"#E52246" }}>Post</Text>
+      </Title>
+
+      <Input
+        placeholder="Seu nome"
+      />
+
+      <Input
+        placeholder="Seuemail@teste.com"
+      />
+
+      <Input
+        placeholder="*****"
+      />
+
+      <Button>
+        <ButtonText>Cadastrar</ButtonText>
+      </Button>
+
+      <SignUpButton onPress={toggleLogin}>
+        <SignUpText>Já possuo uma conta</SignUpText>
+      </SignUpButton>
+
+    </Container>
   );
 }
