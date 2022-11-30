27a1762ab8ccc0e35029c1503bdb907d58e1cc75
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-30 20:34:31 -0300

    Feat: configurado a tabBar com suas icons

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather'
 
 import Home from '../pages/Home'
 import Profile from '../pages/Profile'
@@ -10,11 +11,47 @@ import PostsUser from '../pages/PostsUser'
 const Tab = createBottomTabNavigator();
 
 export default function AppRoutes() {
- return (
-    <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Search' component={Search}/>
-        <Tab.Screen name='Profile' component={Profile}/>
+  return (
+    <Tab.Navigator
+      screenOptions={{  //Configuraçoes de estilo para a tab 
+        headerShown: false,
+        tabBarHideOnKeyboard: true, //para esconde a tab quando abrir o teclado
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#fff',
+
+        tabBarStyle: {
+          backgroundColor: '#202225',
+          borderTopWidth: 0
+        }
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="home" color={color} size={size} />
+          }
+        }}
+      />
+      <Tab.Screen
+        name='Search'
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="search" color={color} size={size} />
+          }
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="user" color={color} size={size} />
+          }
+        }}
+      />
     </Tab.Navigator>
   );
 }
