c6aba4cb3891a4cc08e67723773e0e03ae4d0a4f
fabricio-26 <fabricioohh@gmail.com>
2022-12-01 12:06:53 -0300

    Feat: Home e Header - inserido o flatList na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather'
 
 import { useNavigation } from '@react-navigation/native'
-import { Container, ButtonPost } from './styles';
+import Feather from 'react-native-vector-icons/Feather'
+
+import { Container, ButtonPost, ListPosts } from './styles';
+import Header from '../../components/Header'
 
 export default function Home() {
   const navigation = useNavigation()
 
+  const [posts, setPosts] = useState([
+    {id: '1', content: 'Teste1'},
+    {id: '2', content: 'Teste2'},
+    {id: '3', content: 'Teste3'},
+    {id: '4', content: 'Teste4'}
+  ])
+
  return (
    <Container>
-    <Text>Home</Text>
+    <Header/>
+
+    <ListPosts
+      data={posts}
+      renderItem={ ({item}) => ( <Text>Teste</Text>) }
+      />
 
     <ButtonPost activeOpacity={0.5} onPress={ ()=> navigation.navigate('NewPost')}>
       <Feather name='edit-2' color="#fff" size={25}/>
