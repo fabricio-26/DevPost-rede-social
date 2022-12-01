2281e96190c6b8e68603c6f2a255850a4dde83c3
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-30 23:11:37 -0300

    Feat: criando page para NewPost

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useLayoutEffect } from 'react';
+
+import { useNavigation } from '@react-navigation/native'
+import { Container, Input, Button, ButtonText } from './styles'
 
 export default function NewPost() {
+  const navigation = useNavigation()
+  const [post, setPost] = useState('')
+
+  useLayoutEffect(()=>{
+
+    const options = navigation.setOptions({
+      headerRight: () => (
+        <Button>
+          <ButtonText>Compartilhar</ButtonText>
+        </Button>
+      )
+    })
+
+  },[navigation, post])
+
  return (
-   <View>
-    <Text>Tela NewPost</Text>
-   </View>
+   <Container>
+    <Input
+      placeholder="O que está acontecendo?"
+      value={post}
+      onChangeText={ (text) => setPost(text)}
+      autoCorrect={false}
+      multiline={true}
+      placeholderTextColor='#ddd'
+      maxLength={300}
+    />
+   </Container>
   );
 }
---
+++ src/pages/NewPost/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #404349;
+`;
+
+export const Input = styled.TextInput`
+    background-color: transparent;
+    margin: 10px;
+    color: #FFF;
+    font-size: 20px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    background-color: #418cfd;
+    margin-right: 7px;
+    padding: 5px 12px;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+    color: #fff;
+`;
