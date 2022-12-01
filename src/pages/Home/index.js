ec768e7191089373db243a510be6507111a6440a
fabricio henrique alves <fabricioohh@gmail.com>
2022-11-30 22:37:07 -0300

    Feat: Acrescentado feedButton na page Home para navegacao ate a pagina de criaçao de novo post

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather'
+
+import { useNavigation } from '@react-navigation/native'
+import { Container, ButtonPost } from './styles';
 
 export default function Home() {
+  const navigation = useNavigation()
+
  return (
-   <View>
+   <Container>
     <Text>Home</Text>
-   </View>
+
+    <ButtonPost activeOpacity={0.5} onPress={ ()=> navigation.navigate('NewPost')}>
+      <Feather name='edit-2' color="#fff" size={25}/>
+    </ButtonPost>
+   </Container>
   );
 }
