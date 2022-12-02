2ad0b5adf680f0e6ba2b0d2168cca0edf7dae74f
fabricio-26 <fabricioohh@gmail.com>
2022-12-02 14:16:19 -0300

    Feat: posts do usuario

--- src/pages/PostsUser/index.js
+++ src/pages/PostsUser/index.js
@@ -1,18 +1,56 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect, useState, useCallback } from 'react';
 import { View, Text } from 'react-native';
-import { useRoute, useNavigation } from '@react-navigation/native';
+import { useRoute, useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import firestore from '@react-native-firebase/firestore';
+import { is } from 'date-fns/locale';
 
 export default function PostsUser() {
   const route = useRoute()
   const navigation = useNavigation();
 
   const [title, setTile] = useState(route?.params.title)
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useLayoutEffect(()=>{
     navigation.setOptions({
       title: title === '' ? '' : title
     })
   },[navigation, title])
+
+  useFocusEffect(
+    useCallback(()=>{
+      let isActive = false;
+
+      firestore()
+      .collection('posts')
+      .where('userId', '==', route.params?.userId)
+      .orderBy('created', 'desc')
+      .get()
+      .then((snapshot) => {
+        const postList = [];
+
+        snapshot.docs.map( u => {
+          postList.push({
+            ...u.data(),
+            id: u.id
+          })
+        })
+
+        if(isActive){
+          setPosts(postList)
+          setLoading(false)
+        }
+
+      })
+
+      return ()=>{
+        isActive = false
+      }
+
+    },[])
+  )
 
  return (
    <View>
