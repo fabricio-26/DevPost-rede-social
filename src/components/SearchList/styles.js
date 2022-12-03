b371cfc1c3d4a052a910b0d84102672b332b9048
fabricio henrique alves <fabricioohh@gmail.com>
2022-12-03 15:13:33 -0300

    Feat: finalizado as funcionalidades de busca de usuarios na pagina de Search

---
+++ src/components/SearchList/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.TouchableOpacity`
+    margin: 5PX 10PX;
+    background-color: #222227;
+    padding: 10px;
+    border-radius: 4px;
+`;
+
+export const Name = styled.Text`
+    color: #fff;
+    font-size: 17px;
+`;
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
+
+import firestore from '@react-native-firebase/firestore'
+
+import SearchList from '../../components/SearchList';
+
+import Feather from 'react-native-vector-icons/Feather';
+import { Container, AreaInput, Input, List } from './styles';
 
 export default function Search() {
- return (
-   <View>
-    <Text>Tela Procurar</Text>
-   </View>
+  const [name, setName] = useState('')
+  const [users, setUsers] = useState('')
+
+  useEffect(()=>{
+    if(name === '' || name === undefined){
+      setName([])
+      return;
+    }
+    // forma de fazer input que vusque usuarios digitados e entrega opçoes com o nomes parecidos que foi digitado no input
+    const subscriber = firestore().collection('users')
+    .where('nome', '>=', name)
+    .where('nome', '<=', name + "\uf8ff")
+    .onSnapshot( snapshot => {
+      const listUsers = [];
+
+      snapshot.forEach(doc => {
+        listUsers.push({
+          ...doc.data(),
+          id: doc.id
+        })
+      })
+
+      setUsers(listUsers)
+    })
+
+    return () => subscriber()
+
+  },[name])
+
+
+
+  return (
+    <Container>
+      <AreaInput>
+        <Feather name="search" size={20} color="#e52246" />
+        <Input
+          placeholder="Procurando alguem?"
+          placeholderTextColor="#353840"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+      </AreaInput>
+
+      <List
+        data={users}
+        renderItem={ ({item}) => <SearchList data={item}/>}
+      />
+
+
+    </Container>
   );
 }
