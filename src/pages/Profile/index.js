494da96df36cd315c97456e519789b07bf5e7b90
fabricio henrique alves <fabricioohh@gmail.com>
2022-12-03 19:33:03 -0300

    Feat: pagina de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,24 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import { 
+  Container,
+  Name,
+  Email,
+  Button,
+  ButtonText,
+  UpLoadButton,
+  UpLoadText,
+  Avatar
+ } from './styles'
 
 export default function Profile() {
-  const { signOut } = useContext(AuthContext)
+  const { signOut, user, } = useContext(AuthContext)
+
+  const  [nome, setNome] = useState(user?.nome)
+  const  [url, setUrl] = useState(null)
 
   // ------ Funcao para sair(Deslogar) ----------
   async function handleSignOut(){
@@ -13,12 +27,33 @@ export default function Profile() {
 
 
  return (
-   <View>
-    <Text>Perfil</Text>
-    <Button
-      title='Sair'
-      onPress={handleSignOut}
-    />
-   </View>
+   <Container>
+    <Header/>
+
+    { url ? (
+      <UpLoadButton>
+        <UpLoadText>+</UpLoadText>
+        <Avatar
+          source={{uri: url}}
+        />
+      </UpLoadButton>
+    ) : (
+      <UpLoadButton>
+        <UpLoadText>+</UpLoadText>
+      </UpLoadButton>
+    )}
+
+    <Name>{user?.nome}</Name>
+    <Email>{user?.email}</Email>
+
+    <Button bg="#428cfd">
+      <ButtonText>Atualizar Perfil</ButtonText>
+    </Button>
+
+    <Button bg="#fff" onPress={handleSignOut}>
+      <ButtonText color="#3b3b3b">Sair</ButtonText>
+    </Button>
+
+   </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,4 +1,65 @@
 import styled from "styled-components";
 
-export const Conatainer = styled.SafeAreaView`
+export const Container = styled.View`
+flex: 1;
+align-items: center;
+background-color: #353840;
+`
+
+export const Name = styled.Text`
+margin-top: 20px;
+margin-right: 20px;
+margin-left: 20px;
+font-size: 28px;
+font-weight: bold;
+color: #fff;
+`
+
+export const Email = styled.Text`
+color: #fff;
+margin-top: 10px;
+margin-right: 20px;
+margin-left: 20px;
+font-size: 18px;
+font-style: italic;
+`
+
+export const Button = styled.TouchableOpacity`
+margin-top: 16px;
+background-color: ${props => props.bg};
+width: 80%;
+height: 50px;
+border-radius: 4px;
+align-items: center;
+justify-content: center
+`
+
+export const ButtonText = styled.Text`
+font-size: 18px;
+color: ${props => props.color};
+`
+
+export const UpLoadButton = styled.TouchableOpacity`
+    margin-top: 20%;
+    background-color: #fff;
+    width: 165px;
+    height: 165px;
+    border-radius: 90px;
+    justify-content: center;
+    align-items: center;
+    z-index: 8;
+`
+
+export const UpLoadText = styled.Text`
+    font-size: 55px;
+    position: absolute;
+    color: #e52246;
+    opacity: 0.5;
+    z-index: 99;
+`
+
+export const Avatar = styled.Image`
+    width: 160px;
+    height: 160px;
+    border-radius: 80px;
 `
