def22693fb26a7822d2ee5e37a7f49e70be22ec5
fabricio henrique alves <fabricioohh@gmail.com>
2022-12-02 21:52:02 -0300

    Feat: exibindo todos os posts feitos por um usuario escolhido ao clicar no nome dele no posts

--- src/pages/PostsUser/index.js
+++ src/pages/PostsUser/index.js
@@ -1,13 +1,18 @@
-import React, { useLayoutEffect, useState, useCallback } from 'react';
-import { View, Text } from 'react-native';
+import React, { useLayoutEffect, useState, useCallback, useContext } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { useRoute, useNavigation, useFocusEffect } from '@react-navigation/native';
 
+import { AuthContext } from '../../contexts/auth'
+
 import firestore from '@react-native-firebase/firestore';
-import { is } from 'date-fns/locale';
+
+import PostsList from '../../components/PostsList';
+import { Container, ListPosts } from './styles';
 
 export default function PostsUser() {
   const route = useRoute()
   const navigation = useNavigation();
+  const { user } = useContext(AuthContext)
 
   const [title, setTile] = useState(route?.params.title)
   const [posts, setPosts] = useState([])
@@ -21,7 +26,7 @@ export default function PostsUser() {
 
   useFocusEffect(
     useCallback(()=>{
-      let isActive = false;
+      let isActive = true;
 
       firestore()
       .collection('posts')
@@ -53,8 +58,18 @@ export default function PostsUser() {
   )
 
  return (
-   <View>
-    <Text>{route.params?.title}</Text>
-   </View>
+   <Container>
+    {loading ? (
+      <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+        <ActivityIndicator color="#e52246" size={50}/>
+      </View>
+    ) : (
+      <ListPosts
+        showsVerticalScrollIndicator={false}
+        data={posts}
+        renderItem={ ({item}) => <PostsList data={item} userId={user.uid}/>}
+      />
+    )}
+   </Container>
   );
 }
